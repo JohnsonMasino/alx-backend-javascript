412af33c94dd58c3ef386b81addb7b23d2b09793
JohnsonMasino <johnsonmasino@gmail.com>
2023-09-28 16:59:18 +0000

    File 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
-
+/* disabling eslint */
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-   
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
