2ba556ce56600b315f111c84be85b7d5782e31aa
JohnsonMasino <johnsonmasino@gmail.com>
2023-09-28 23:40:25 +0000

    File 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,50 +1,15 @@
-import Currency from './3-currency';
-
-export default class Pricing {
-  constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+          && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._amount = amount;
 
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
-    this._currency = currency;
+    this._sqft = sqft;
   }
 
-  // Getter and setter for amount
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(newAmount) {
-    if (typeof newAmount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = newAmount;
-  }
-
-  // Getter and Setter method for currency
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of Currency');
-    }
-    this._currency = newCurrency;
-  }
-
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
-
-  static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Amount and conversion rate must be numbers');
-    }
-    return amount * conversionRate;
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
   }
 }
